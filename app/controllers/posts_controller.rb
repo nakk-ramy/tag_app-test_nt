51d94484953acd9fa86404811c9d92075855b22a
nakk-ramy <nakki.7015.alan@gmail.com>
2022-08-18 12:26:34 +0900

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post_form = PostForm.new
+  end
+
+  def create
+    @post_form = PostForm.new(post_form_params)
+    if @post_form.valid?
+      @post_form.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def post_form_params
+    params.require(:post_form).permit(:text, :image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
---
+++ app/models/post_form.rb
@@ -0,0 +1,15 @@
+class PostForm
+  include ActiveModel::Model
+
+
+  attr_accessor :text, :image
+
+  with_options presence: true do
+    validates :text
+    validates :image
+  end
+
+  def save
+    Post.create(text: text, image: image)
+  end
+end
