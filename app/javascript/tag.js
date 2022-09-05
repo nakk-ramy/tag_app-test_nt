f2e60c66dba4ab9f720b698ec0ff21f0a58517f6
nakk-ramy <nakki.7015.alan@gmail.com>
2022-09-05 20:34:45 +0900

    非同期通信を行うまでの処理

---
+++ app/javascript/tag.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+const tagNameInput = document.querySelector("#post_form_tag_name");
+  if (tagNameInput){
+    const inputElement = document.getElementById("post_form_tag_name");
+    inputElement.addEventListener("input", () => {
+      const keyword = document.getElementById("post_form_tag_name").value;
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `/posts/search/?keyword=${keyword}`, true);
+      XHR.responseType = "json";
+      XHR.send();
+    });
+  };
+});
